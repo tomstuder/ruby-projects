8bc47be168bc0edebb4a9e834bfa81e83bdc1409
tomstuder <tom@tomstuder.com>
2018-08-20 13:57:55 -0400

    new class and loop projects

---
+++ 01/class.rb
@@ -0,0 +1,16 @@
+class Greeter
+  def initialize(name = "World")
+    @name = name
+  end
+  def say_hi
+    puts "Hi #{@name}!"
+  end
+    def say_bye
+    puts "Bye #{@name}, come back soon."
+  end
+end
+
+
+greeter = Greeter.new("Tom")
+greeter.say_hi
+greeter.say_bye
